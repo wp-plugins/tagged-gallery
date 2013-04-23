913625725c492d05b151f7a5efeb1072736fb52e
bergh <bergh@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-04-23 18:31:53 +0000

    0.6 Thumbs are now generated by jQuery and not phpimagick
    git-svn-id: https://plugins.svn.wordpress.org/tagged-gallery/trunk@702413 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/resize.js
@@ -0,0 +1,7 @@
+$j=jQuery.noConflict();
+$j(document).ready(function() {
+            $j('.tagged-gallery').resizecrop({
+                    width: '150',
+                    height: '150'
+                });
+            });  
